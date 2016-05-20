58a75b5a19d06e98145a365077d36ad3e13d2ddd
Marius <marius.kildedal@gmail.com>
2016-05-20 12:35:22 +0200

    start with a black slate

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -15,65 +15,24 @@ var bio = {
   "skills" : ["awsomness", "wireless", "network" ]
 };
 
-var name = bio.name;
-var formattedName = HTMLheaderName.replace("%data%", name);
+var work = {};
+  work.position = "Consultant";
+  work.employer  = "Atea";
+  work.years  = 6;
+  work.city = "Kristiansand";
 
-var role = bio.role;
-var formattedRole = HTMLheaderRole.replace("%data%", role);
+var education = {};
+  education["name"] = "Noroff";
+  education["degree"] = "System Admin";
+  education["years"] = 1;
+  education["city"] = "Kristiansand";
 
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
-
-var mobile = bio.contacts.mobile;
-var formattedMobile = HTMLmobile.replace("%data%", mobile);
-$("#topContacts").append(formattedMobile);
-
-var email = bio.contacts.email;
-var formattedEmail = HTMLemail.replace("%data%", email);
-$("#topContacts").append(formattedEmail);
-
-var github = bio.contacts.github;
-var formattedGithub = HTMLgithub.replace("%data%", github);
-$("#topContacts").append(formattedGithub);
-
-var twitter = bio.contacts.twitter;
-var formattedTwitter = HTMLtwitter.replace("%data%", twitter);
-$("#topContacts").append(formattedTwitter);
-
-var _location = bio.contacts.location;
-var formattedLocation = HTMLlocation.replace("%data%", _location);
-$("#topContacts").append(formattedLocation);
-
-var bioPic = bio.bioPic;
-var formattedBioPic = HTMLbioPic.replace("%data%", bioPic);
-$("#header").append(formattedBioPic);
-
-var welcomeMsg = bio.welcomeMsg;
-var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", welcomeMsg);
-$("#header").append(formattedWelcomeMsg);
-
-var skills = bio.skills;
-var formattedSkills = HTMLskills.replace("%data%", skills);
-$("#header").append(HTMLskillsStart);
-$("#skills").append(formattedSkills);
-
-$("#workExperience").append(HTMLworkStart);
-
-var work = {
-  "job_position" : "Consultant",
-  "employer" : "Atea",
-  "years" : 6,
-  "city" : "Kristiansand"
+var projects = {
+  "projects": [
+    {
+      "title": "Sample 1",
+      "dates": "2016",
+      "description": "sample projects on udacity",
+    }
+  ]
 };
-
-var position = work.job_position;
-var formattedPostion = HTMLworkTitle.replace("%data%", position);
-$("#workExperience").append(formattedPostion);
-
-var employer = work.employer;
-var formattedEmployer = HTMLworkEmployer.replace("%data%", employer);
-$("#workExperience").append(formattedEmployer);
-
-var years = work.years;
-var formattedYears = HTMLworkDates.replace("%data%", years);
-$("#workExperience").append(formattedYears);
